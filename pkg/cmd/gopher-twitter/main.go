e3becc0f5b215793e21cea229464e5371f5d7d42
root <test@test.com>
2019-12-22 23:07:47 +0300

    Implement simple web server

--- pkg/cmd/gopher-twitter/main.go
+++ pkg/cmd/gopher-twitter/main.go
@@ -1,1 +1,21 @@
-package gopher_twitter
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"os"
+)
+
+func main() {
+	router := mux.NewRouter()
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	err := http.ListenAndServe(":" + port, router)
+	if err != nil {
+		fmt.Print(err)
+	}
+}

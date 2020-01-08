876ca01d58ee79e9e2e32db6b0d1eaa9605585cf
root <test@test.com>
2020-01-08 15:54:35 +0300

    Implement ShowTweets controller

--- internal/app/controllers/tweetController.go
+++ internal/app/controllers/tweetController.go
@@ -31,3 +31,32 @@ var CreateTweet = func(w http.ResponseWriter, r *http.Request) {
 	response := tweet.Create()
 	u.Response(w, response)
 }
+
+var ShowTweets = func(w http.ResponseWriter, r *http.Request) {
+	us := &models.User{}
+	us, err := us.GetById(r.Context().Value("user").(uint))
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		u.Response(w, u.Message(false, "User not found"))
+	}
+
+	sub := &models.Subscriber{User: us}
+	subscriptions, err := sub.GetSubscriptions()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		u.Response(w, u.Message(false, "Internal server error"))
+	}
+
+	var userIds []uint
+	for _, v := range subscriptions {
+		if v.Status == true {
+			userIds = append(userIds, v.SubscribedId)
+		}
+	}
+
+	t := models.Tweet{}
+	tweets, err := t.GetByUserIds(userIds, 30)
+	response := u.Message(true, "Tweets from your subscription list")
+	response["tweets"] = tweets
+	u.Response(w, response)
+}

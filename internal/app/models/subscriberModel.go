9fbe5d188a632ce2f0aa599fc1c32ee99af390f8
root <test@test.com>
2020-01-08 13:36:33 +0300

    Create Subscriber model

---
+++ internal/app/models/subscriberModel.go
@@ -0,0 +1,52 @@
+package models
+
+import (
+	"fmt"
+	"github.com/Shyp/go-dberror"
+	"github.com/flatropw/gopher-twitter/internal/app/db"
+	"time"
+)
+
+type Subscriber struct {
+	user *User
+}
+
+type Subscription struct {
+	Id uint
+	Status bool
+}
+
+func (sub *Subscriber) SubscribeTo(Id uint) (subscription *Subscription, err error) {
+	subscription, err = sub.HasAlreadySubscribed(Id)
+	if err != nil {
+		return
+	}
+
+	if subscription.Id > 0 {
+		_, err = db.Instance.Db.Exec(db.SubUpdateStatusQuery, !subscription.Status, time.Now().Unix(), subscription.Id)
+	} else {
+		row := db.Instance.Db.QueryRow(db.SubInsertQuery, sub.user.Id, Id, time.Now().Unix(), time.Now().Unix())
+		err = row.Scan(&subscription.Id, &subscription.Status)
+	}
+	fmt.Println(err)
+	dbErr := dberror.GetError(err)
+	switch e := dbErr.(type) {
+	case *dberror.Error:
+		return &Subscription{}, fmt.Errorf(e.Error())
+	default:
+		return
+	}
+}
+
+func (sub *Subscriber) HasAlreadySubscribed(Id uint) (*Subscription, error) {
+	var subscription = Subscription{}
+	row := db.Instance.Db.QueryRow(db.SubHasAlreadySubscribedQuery, sub.user.Id, Id)
+	err := row.Scan(&subscription.Id, &subscription.Status)
+	dbErr := dberror.GetError(err)
+	switch e := dbErr.(type) {
+	case *dberror.Error:
+		return &Subscription{}, fmt.Errorf(e.Error())
+	default:
+		return &subscription, nil
+	}
+}
